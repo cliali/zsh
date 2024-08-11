83a6be5128b7a0c26fbe5107da9b22fbc6f02e66
Ali <cliali1181@gmail.com>
2024-08-11 19:41:43 +0330

    fix typo

--- zsh.sh
+++ zsh.sh
@@ -21,7 +21,7 @@ if [ -n "$missing_tools" ]; then
 fi
 
 
-rm -rf ~/.oh-my-zddsh
+rm -rf ~/.oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
